07821626f1eabe905ac30d62022d7eeb7c425880
kevin-gjm <gjm_kevin@163.com>
2016-08-12 16:28:35 +0800

    eventloop some small change

--- src/net/EventLoop.cc
+++ src/net/EventLoop.cc
@@ -5,22 +5,44 @@
 //
 // Author: Shuo Chen (chenshuo at chenshuo dot com)
 
-
+#pragma comment( lib, "ws2_32.lib"  )  // for select 
+#pragma comment(lib,"ws2_32")
+#include <WinSock2.h>
 #include "EventLoop.h"
 
 #include "logging.h"
-
 #include <assert.h>
-//#include <WinSock2.h>
+#include <WinBase.h>
+#include <direct.h>
 
+#include <WS2tcpip.h>
+#include <Windows.h>
 using namespace calm;
 
 thread_local EventLoop* t_loopInThisThread = 0;
+
+int initWinSock()
+{
+	WSADATA wsaData;
+	WORD wVersionRequested = MAKEWORD(1, 1);
+	if (0 == WSAStartup(wVersionRequested, &wsaData))
+	{
+		LOG_INFO << "RUN-MSG:Success to connect to the lib of socket!";
+		return 0;
+	}
+	else
+	{
+		LOG_INFO << "ERR-MSG:Failed to connect to the lib of socket!";
+		return -1;
+	}
+	//wVersionRequested是我们要求使用的WinSock的版本。
+}
 
 EventLoop::EventLoop()
   : looping_(false),
     threadId_(calm::getCurrentThreadId())
 {
+	initWinSock();
   LOG_TRACE << "EventLoop created " << this << " in thread " << threadId_;
   if (t_loopInThisThread)
   {
@@ -45,20 +67,20 @@ void EventLoop::loop()
   assertInLoopThread();
   looping_ = true;
 
-  //timeval timeout;
-  //timeout.tv_sec = 1;	//wait_timeout 1 second
-  //timeout.tv_usec = 0;
+  timeval timeout;
+  timeout.tv_sec = 10;	//wait_timeout 1 second
+  timeout.tv_usec = 0;
 
-  //fd_set read_set, write_set, excep_set;
-  //FD_ZERO(&read_set);
-  //FD_ZERO(&write_set);
-  //FD_ZERO(&excep_set);
-  //while (looping_)
-  //{
-	 // int nfds = select(0, &read_set, &write_set, &excep_set, &timeout);
-  //}
-  while(looping_)
-  { }
+  fd_set read_set, write_set, excep_set;
+  FD_ZERO(&read_set);
+  FD_ZERO(&write_set);
+  FD_ZERO(&excep_set);
+
+  int nfds = 0;
+  do {
+	  nfds  = select(0, NULL, NULL, NULL, &timeout);
+  } while (nfds < 0 && errno == EINTR);
+
   LOG_TRACE << "EventLoop " << this << " stop looping";
   looping_ = false;
 }
