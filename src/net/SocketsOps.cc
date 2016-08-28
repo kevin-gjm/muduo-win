565c97eb2b9bbeaab2947cd8ad49b3a0c9bcebe5
kevin-gjm <gjm_kevin@163.com>
2016-08-28 16:52:56 +0800

    Temporary storage without compile

---
+++ src/net/SocketsOps.cc
@@ -0,0 +1,40 @@
+#include <SocketsOps.h>
+#include <logging.h>
+#include <types.h>
+#include <Endian.h>
+
+#include <errno.h>
+#include <stdio.h>
+
+#pragma comment( lib, "ws2_32.lib"  )  // for select 
+#pragma comment(lib,"ws2_32")
+#include <WinSock2.h>
+
+#ifdef _WIN32
+#define snprintf	sprintf_s
+#endif
+
+using namespace calm;
+using namespace calm::net;
+
+namespace
+{
+	typedef struct sockaddr SA;
+
+	void setNonBlock(int sockfd)
+	{
+#ifdef _MSC_VER
+		u_long nonblock = 1;
+		int ret = ioctlsocket(sockfd, FIONBIO, &nonblock);
+#else
+		int ret = fcntl(fd, F_SETFL, O_NONBLOCK | fcntl(fd, F_GETFL));
+#endif
+		if (ret == SOCKET_ERROR)
+		{
+			//LOG__(NET, _T("_SetNonblock failed, err_code=%d"), _GetErrorCode());
+		}
+	}
+} //
+
+
+
---
+++ src/net/SocketsOps.h
@@ -0,0 +1,50 @@
+#ifndef CALM_NET_SOCKETSOPS_H_
+#define CALM_NET_SOCKETSOPS_H_
+
+#include <stdint.h>
+#include <stdio.h>
+
+#ifdef _MSC_VER
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#endif // _MSC_VER
+
+
+namespace calm
+{
+	namespace net
+	{
+		namespace sockets
+		{
+			int createNoneblockOrDie(int family);
+			int connect(int socketfd, const struct sockaddr* addr);
+			void bindOrDie(int sockfd, const struct sockaddr* addr);
+			void listenOrDie(int sockfd);
+			
+			int accept(int sockfd, struct sockaddr_in* addr);
+
+			ssize_t read(int sockfd, void *buf, size_t count);
+			ssize_t readv(int sockfd, const struct iovec *iov, int iovcnt);
+			ssize_t write(int sockfd, const void* buf, size_t count);
+
+			void close(int sockfd);
+			void shutdownWrite(int sockfd);
+			void toIpPort(char* buf, size_t size, const struct sockaddr* addr);
+			void toIp(char* buf, size_t size, const struct sockaddr* addr);
+
+			void fromIpPort(const char* ip, uint16_t port, struct sockaddr_in* addr);
+
+			int getSockerError(int sockfd);
+
+			const struct sockaddr* sockaddr_cast(const struct sockaddr_in* addr);
+			const struct sockaddr_in* sockaddr_in_cast(const struct sockaddr* addr);
+
+			struct sockaddr_in getLocalAddr(int sockfd);
+			struct sockaddr_in getPeerAddr(int sockfd);
+
+			bool isSelfConnect(int sockfd);
+		}// end namespace sockets
+	}// end namespace net
+}// end namespace calm
+
+#endif //CALM_NET_SOCKETSOPS_H_
